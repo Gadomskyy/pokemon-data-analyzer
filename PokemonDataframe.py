8a74e8bd173dff280e6f3d76169b0f5ff9c16edc
Gadomskyy <j_gadomski@hotmail.com>
2024-02-21 21:42:27 +0100

    add only abilities name for ability column

--- PokemonDataframe.py
+++ PokemonDataframe.py
@@ -9,14 +9,36 @@ def get_pokemon_data(number):
     data = response.json()
     return data
 
+def get_ability_names(pokemon):
+    abilities_df = pd.json_normalize(pokemon['abilities'])
+    ability_names = abilities_df['ability.name'].tolist()
+    return ability_names
+
 
 def all_pokemon_data_to_df():
     df = pd.DataFrame(
-        columns=['id', 'name', 'height', 'weight', 'abilities', 'forms', 'moves', 'stats', 'types'])
+        columns=['id',
+                 'name',
+                 'height',
+                 'weight',
+                 'forms',
+                 'moves',
+                 'stats',
+                 'types',
+                 'abilities'])
     for i in range(1, 10):
         pokemon = get_pokemon_data(i)
         pokemon_normalized = pd.json_normalize(pokemon)[
-            ['id', 'name', 'height', 'weight', 'abilities', 'forms', 'moves', 'stats', 'types']]
+            ['id',
+             'name',
+             'height',
+             'weight',
+             'forms',
+             'moves',
+             'stats',
+             'types']]
+        pokemon_normalized['abilities'] = [get_ability_names(pokemon)]
         df = pd.concat([df, pokemon_normalized])
     return df
 
+all_pokemon_data_to_df().to_excel("result.xlsx")
