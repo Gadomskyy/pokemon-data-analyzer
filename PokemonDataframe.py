d18f37e121445335a5ece7d4acfe1d0dedfdbf88
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2024-02-27 14:17:04 +0100

    Put stats into seperate columns (#5)
    * remove id column, make index serve as id
    
    * change stats from one column to seperate columns
    
    * remove get_stats function
    
    * add pokemon number in all_pokemon_data_to_df for clarity
    
    * change column order
    
    * add attack column

--- PokemonDataframe.py
+++ PokemonDataframe.py
@@ -9,44 +9,60 @@ def get_pokemon_data(number):
     data = response.json()
     return data
 
+
 def get_attribute_names(pokemon, attribute, attribute2):
     df = pd.json_normalize(pokemon[attribute])
     attribute_names = df[f'{attribute2}.name'].tolist()
     return attribute_names
+
 
 def get_forms(pokemon):
     df = pd.json_normalize(pokemon['forms'])
     forms = df['name'].tolist()
     return forms
 
-def get_stats(pokemon):
+
+def get_stat(pokemon, name):
     df = pd.json_normalize(pokemon['stats'])
-    dict_list = [{'stat': stat, 'value': value} for stat, value in zip(df['stat.name'], df['base_stat'])]
-    return dict_list
+    stat = df.loc[df['stat.name'] == name, 'base_stat'].iloc[0]
+    return stat
 
 
 def all_pokemon_data_to_df():
+    pokemon_number = 1025
+
     df = pd.DataFrame(
         columns=['name',
                  'height',
                  'weight',
-                 'stats',
+                 'hp',
+                 'attack',
+                 'defense',
+                 'special-attack',
+                 'special-defense',
+                 'speed',
                  'abilities',
                  'types'])
-    for i in range(1, 10):
+    for i in range(1, pokemon_number + 1):
         pokemon = get_pokemon_data(i)
         pokemon_normalized = pd.json_normalize(pokemon)[
             ['name',
              'height',
              'weight']]
-        pokemon_normalized['abilities'] = [get_attribute_names(pokemon, 'abilities', 'ability')]
-        pokemon_normalized['types'] = [get_attribute_names(pokemon, 'types', 'type')]
         pokemon_normalized['moves'] = [get_attribute_names(pokemon, 'moves', 'move')]
         pokemon_normalized['forms'] = [get_forms(pokemon)]
-        pokemon_normalized['stats'] = [get_stats(pokemon)]
+        pokemon_normalized['hp'] = get_stat(pokemon, 'hp')
+        pokemon_normalized['attack'] = get_stat(pokemon, 'attack')
+        pokemon_normalized['defense'] = get_stat(pokemon, 'defense')
+        pokemon_normalized['special-attack'] = get_stat(pokemon, 'special-attack')
+        pokemon_normalized['special-defense'] = get_stat(pokemon, 'special-defense')
+        pokemon_normalized['speed'] = get_stat(pokemon, 'speed')
+        pokemon_normalized['abilities'] = [get_attribute_names(pokemon, 'abilities', 'ability')]
+        pokemon_normalized['types'] = [get_attribute_names(pokemon, 'types', 'type')]
         df = pd.concat([df, pokemon_normalized])
         df.reset_index(drop=True, inplace=True)
         df.index += 1
         df.index.rename('id', inplace=True)
     return df
 
+all_pokemon_data_to_df().to_excel("result.xlsx")
